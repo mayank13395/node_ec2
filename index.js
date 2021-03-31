0527341764182a95204a7fc75aa8ecee195574cc
mayank13395 <kumar.mayank13395@gmail.com>
2021-03-31 10:06:33 +0530

    second commit

--- index.js
+++ index.js
@@ -8,12 +8,12 @@ app.use(cors());
 
 app.get('/', (req, res) => {
   res.status(200).json({
-      message:"hello from aws ec2 instance"
+      message:"hello from aws ec2 instance 2"
   })
 });
 
 
 app.listen(5000, () => {
-    console.log("microservice-1 is listening on port 5000");
+    console.log("microservice-2 is listening on port 6000");
 });
 
